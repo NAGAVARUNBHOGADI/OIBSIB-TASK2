78cad6e87e60719a80e3ebad190ad6878823b331
NAGA VARUN BHOGADI <85406841+NAGAVARUNBHOGADI@users.noreply.github.com>
2023-02-05 11:09:32 +0530

    Update NumberGuessing.java

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -1,89 +1,141 @@
+import java.io.*;
 import java.util.*;
+import java.lang.Math;
 
-public class NumberGuessing {
-    static ArrayList<Integer> totalScore = new ArrayList<Integer>();
+class RandomNumber{
 
-    public static void main(String[] args) {
-        NumberGuessing options = new NumberGuessing();
-        options.menu(totalScore);
-    }
+	static int levels=0;
+	static int Number_of_attempts=0;
+	static double score=0;
+	static int low,mid=0,high=100;
+	
+	public int randomNumber(){
+		
+			return (int)Math.floor(Math.random()*101);
 
-    public void menu(ArrayList<Integer> totalScore) {
-        NumberGuessing options = new NumberGuessing();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("----------------------------------");
-        System.out.println("Welcome to the number game");
-        System.out.println("1) Play the Game");
-        System.out.println("2) Score Board");
-        System.out.println("3) Exit the game");
-        System.out.println("-----------------------------------");
-        try {
-            System.out.print("What action would you like to do from the above actions? ");
-            int menuOption = sc.nextInt();
-            switch (menuOption) {
-                case 1:
-                    System.out.print("\n" + "What would you like the range of the numbers to be? ");
-                    int numberRange = sc.nextInt();
-                    int randomNumber = options.randomNumber(numberRange);
-                    options.guessNumber(randomNumber);
-                    break;
-                case 2:
-                    options.displaytotalScore();
-                    break;
-                case 3:
-                    System.out.println("\n" + "Thanks for playing the game!");
-                    System.exit(1);
-                    break;
-                default:
-                    throw new InputMismatchException("Invalid number entry.Could you please Try again later!!!!!!!!!");
-            }
-        } catch (InputMismatchException e) {
-            System.err.println("\n" + e.getMessage() + "\n");
-            menu(totalScore);
-        }
-    }
+	}
+	public void totalScore(){
 
-    public int randomNumber(int numberRange) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(numberRange) + 1;
-        return randomNumber;
-    }
+			System.out.println("<*>=<*>=<*>=<*><*>=<*>=<*>=<*>= yours Score is ***" +score+ "*** <*>=<*>=<*>=<*><*>=<*>=<*>=<*>");
 
-    public void guessNumber(int randomNumber) {
-        Scanner sc = new Scanner(System.in);
-        int userGuess;
-        int guess = 0;
-        do {
-            System.out.print("Enter your guess number: ");
-            userGuess = sc.nextInt();
-            guess++;
-            if (userGuess > randomNumber) {
-                System.out.println("guess the number lower/lesser than previous");
-            } else if (userGuess < randomNumber) {
-                System.out.println("guess the number higher/greater than previous");
-            }
-        } while (randomNumber != userGuess);
-        System.out.println(" ");
-        if (guess == 1) {
-            System.out.println("You answered number is right in " + guess + " try!");
-        } else {
-            System.out.println("You answered number is right in " + guess + " tries!");
-        }
-        totalScore.add(guess);
-        System.out.println(" ");
-        menu(totalScore);
-    }
+			System.out.println("===============level "+levels+" =====================");
 
-    public void displaytotalScore() {
-        System.out.println("......................");
-        System.out.println("Score Board");
-        System.out.println("..........................");
-        System.out.println("Your fastest games today out of all tries is: " + "\n");
-        Collections.sort(totalScore);
-        for (Integer scores : totalScore) {
-            System.out.println("Finished the number game in " + scores + " tries");
-        }
-        System.out.println(" ");
-        menu(totalScore);
-    }
+	}
+
+
+	public boolean game(int userguess,int rn){
+			
+
+				Number_of_attempts++;
+
+				if(userguess>100 || userguess<0){
+
+						System.out.println("the number is between the 0 to 100");
+				
+				}
+				else if(rn==userguess){
+
+						score=score+(1.0/Number_of_attempts)*20;
+
+						levels++;
+
+						return true;
+
+				}
+				else{
+					
+					mid=(low+high)/2;
+
+					System.out.println("generated number is Lower or higher then "+mid);
+
+					if(mid<rn)
+					{		
+							System.out.println("Higher");
+
+							low=mid;
+					
+					}
+					else if(mid>rn)
+					{
+					
+							System.out.println("Lower");
+
+							high=mid;
+					}
+				}
+
+				System.out.println("Number of attempts  "+ Number_of_attempts);
+				
+				System.out.println(rn);
+
+				return false;
+	}
+
+	public static void main(String[] args){
+
+			Scanner input=new Scanner(System.in);
+
+			System.out.println("==============WELCOME TO GAME==============");
+			
+			RandomNumber rd=new RandomNumber();
+
+			int rn=rd.randomNumber();
+			
+			boolean option=true;
+
+			do{
+
+				System.out.println("===============level "+levels+" =====================");
+	
+				System.out.println("sytem generated the random (0,100)\n");
+			
+				while(Number_of_attempts<5){
+				
+					System.out.println("guess the system generated number between (0,100)");
+			
+					int ug=input.nextInt();
+
+					if(rd.game(ug,rn)){
+
+						System.out.println("currently yours Score "+score);
+					}
+			
+				}
+				if(Number_of_attempts==5){
+
+						System.out.println("level failed start from the beggining\n");
+
+						System.out.println("<*>=<*>=<*>=<*><*>=<*>=<*>=<*>=*** yours Score is ***"+score+"<*>=<*>=<*>=<*><*>=<*>=<*>=<*>");
+						
+						levels=0;
+
+				}
+
+				Number_of_attempts=0;
+
+				mid=0;
+
+				low=0;
+
+				high=100;
+
+				System.out.println("Do you wants to continue\nselect the option");
+
+				System.out.print("1. continue\n2. Do you wants to display your score.\n3. exit\n");
+
+				int pt=input.nextInt();
+
+				if(pt==2)
+
+						rd.totalScore();
+
+				else if(pt==3)
+
+						option=false;
+		
+		}while(option);
+	
+
+	}
+
 }
